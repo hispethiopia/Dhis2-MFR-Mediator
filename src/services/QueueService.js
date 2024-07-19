36b4abc15b59e4deeaaed902aab44b54fbf2cfe9
Mikee131 <michaeleshete9@gmail.com>
2024-07-19 18:59:13 +0300

    Dhis2 service updated

--- src/services/QueueService.js
+++ src/services/QueueService.js
@@ -67,7 +67,7 @@ latestUpdatedQueue.process(async (payload, done) => {
 
                             if (isPHCU === true) {
                                 winston.info(`Parent facility ${parentFacilityId} of facility ${mfrFacility.id} is a PHCU.`);
-                            } 
+                            }
                         }
                     }
 
@@ -115,34 +115,35 @@ async function _writeLastDateFile(lastDate) {
 
 DHIS2Service.prototype.getFacilitiesByMfrIds = async function (mfrIds) {
     try {
-        const response = await axios.get(`${process.env.DHIS2_HOST}/organisationUnits`, {
-            params: {
-                fields: 'name,id,attributeValues,lastUpdated',
-                'filter[0]': `attributeValues.attribute.id:eq:${process.env.DHIS2_ATTRIBUTE_ID}`,
-                'filter[1]': `attributeValues.value:in:[${mfrIds.join(',')}]`
-            },
-            auth: {
-                username: process.env.DHIS2_USER,
-                password: process.env.DHIS2_PASSWORD
+        const filters =[`attributeValues.attribute.id:eq:${process.env.DHIS2_ATTRIBUTE_ID}`,
+        `attributeValues.value:in:[${mfrIds.join(',')}]`]
+        const dhisUrl = `${process.env.DHIS2_HOST}/organisationUnits?`+encodeURIComponent(`fields=name,id,attributeValues,lastUpdated&${filters.map(filter => `filter=${filter}`).join('&')}`)
+        // console.log(dhisUrl);
+        
+        const response = await axios.get(dhisUrl, {
+           
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`${process.env.DHIS2_USER}:${process.env.DHIS2_PASSWORD}`).toString('base64')}`
             }
         });
+        // console.log("success",response.data.organisationUnits.length);
         return response.data.organisationUnits;
+    
     } catch (error) {
+        console.log(error);
         winston.error(`Error fetching facilities from DHIS2: ${error.message}`);
         throw error;
     }
 };
 DHIS2Service.prototype.getFacilityByMfrId = async function (mfrId) {
     try {
-        const response = await axios.get(`${process.env.DHIS2_HOST}/organisationUnits`, {
-            params: {
-                fields: 'name,id,attributeValues,lastUpdated',
-                'filter[0]': `attributeValues.attribute.id:eq:${process.env.DHIS2_ATTRIBUTE_ID}`,
-                'filter[1]': `attributeValues.value:eq:${mfrId}`
-            },
-            auth: {
-                username: process.env.DHIS2_USER,
-                password: process.env.DHIS2_PASSWORD
+        const filters =[`attributeValues.attribute.id:eq:${process.env.DHIS2_ATTRIBUTE_ID}`,
+        `attributeValues.value:eq:[${mfrId}]`]
+        const dhisUrl = `${process.env.DHIS2_HOST}/organisationUnits?`+encodeURIComponent(`fields=name,id,attributeValues,lastUpdated&${filters.map(filter => `filter=${filter}`).join('&')}`)
+
+        const response = await axios.get(dhisUrl, {
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`${process.env.DHIS2_USER}:${process.env.DHIS2_PASSWORD}`).toString('base64')}`
             }
         });
         return response.data.organisationUnits;
@@ -166,17 +167,17 @@ module.exports.webhookQueue = webhookQueue;
 const syncSingleFacility = async (payload, done) => {
     try {
         const id = payload.data.id;
-        winston.info("started single facility sync: "+ id)
+        winston.info("started single facility sync: " + id)
         const mfrService = new MFRService();
         const facility = await mfrService.getSingleMFRFacilty(id);
         const dhis2Service = new DHIS2Service();
         const dhis2OrgUnit = await dhis2Service.getFacilityByMfrId(id);
-        const attributeId = process.env.DHIS2_ATTRIBUTE_ID;        
-         if (dhis2OrgUnit.length !== 0){
-        
+        const attributeId = process.env.DHIS2_ATTRIBUTE_ID;
+        if (dhis2OrgUnit.length !== 0) {
+
             await dhis2Service.saveFacilityToDataStore(facility);
-            winston.info("Facility object mapping for DHIS2 "+ facility.name)
-               
+            winston.info("Facility object mapping for DHIS2 " + facility.name)
+
         }
         throw winston.Error('Unable to find faclity in dhis2')
     } catch (err) {
